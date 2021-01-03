46e8ebf9ba873d6bcbb241053962160f04d0ace0
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2021-01-03 04:41:35 -0800

    Update and rename sample_config.py to config.py

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -15,17 +15,17 @@ class Config(object):
     # REQUIRED
     #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = 'OnePunchSupport'  #Your own group for support, do not add the @
-    JOIN_LOGGER = -1001253661229  #Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = -1001190806654  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    API_ID = 1733305  # integer value, dont use ""
+    API_HASH = "f423cffca6b5b7247b31b5b0df61f48d"
+    TOKEN = "1437995498:AAHoCXjc6UQkR5EkewLStlR17acaUoPkQFc"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1156597097  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "MrDani"
+    SUPPORT_CHAT = 'lkhitech'  #Your own group for support, do not add the @
+    JOIN_LOGGER = -1001445108231  #Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = -1001445108231  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     #RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'something://somewhat:user@hosturl:port/databasename'  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = 'postgresql://mrdani:dkkaj0123@postgresql/postgres'  # needed for any database modules
     LOAD = []
     NO_LOAD = ['rss', 'cleaner', 'connection', 'math']
     WEBHOOK = False
